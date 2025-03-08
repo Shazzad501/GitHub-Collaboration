775660a3cef1e28dd6028236d73e8e38dd184640
Shazzad501 <assazzad501@gmail.com>
2025-03-08 22:12:06 +0600

    added by maruf (maruf.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Maruf from "./Components/Maruf";
 import MdAlamin from "./Components/MdAlamin";
 import Rony from "./Components/Rony";
 
@@ -10,12 +11,10 @@ function App() {
         </h1>
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
           <MdAlamin />
-<<<<<<< HEAD
           {/* Nested your component */}
-=======
           {/* Copy Paste your profile component */}
           <Rony></Rony>
->>>>>>> 6c0a7cefc78103ace1a6d84b799d166187e814c9
+          <Maruf/>
         </section>
       </div>
     </>
---
+++ src/Components/Maruf.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import me from '../assets/me .png'
+const Maruf = () => {
+  return (
+    <div>
+      <img src={me} alt="maruf" />
+      <h1>hello i am maruf.</h1>
+    </div>
+  );
+};
+
+export default Maruf;
